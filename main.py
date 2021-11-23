8d7fe156e1458fa839b62cfec8b2f611937dd558
Paskal <Vaxxo9000@gmail.com>
2021-11-23 21:53:07 +0700

    add intersection func

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def inters_size(x1, y1, x2, y2, x3, y3, x4, y4):
+    if (res := (min(y1, y3) - max(y2, y4)) * (min(x4, x2) - max(x1, x3))) < 0:
+        return 0
+    else:
+        return res
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
